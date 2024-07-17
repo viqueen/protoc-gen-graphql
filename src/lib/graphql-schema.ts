fc691d6e79ee2cfb6f48e4cab253170f8126c59e
viqueen <hasnae.rehioui@safetyculture.io>
2024-07-17 18:29:56 +1000

    prepare for generating queries and mutations

--- src/lib/graphql-schema.ts
+++ src/lib/graphql-schema.ts
@@ -35,9 +35,17 @@ interface GraphQLEnum {
     values: string[];
 }
 
+interface GraphQLEndpoint {
+    name: string;
+    inputType: string;
+    outputType: string;
+}
+
 interface GraphQLSchema {
     types: GraphQLType[];
     enums: GraphQLEnum[];
+    queries: GraphQLEndpoint[];
+    mutations: GraphQLEndpoint[];
 }
 
 const asFieldType = (field: FieldDescriptorProto): string => {
@@ -122,16 +130,42 @@ const enumsToSchemaString = (enums: GraphQLEnum[]): string => {
         .join('\n\n');
 };
 
+const endpointsToSchemaString = (endpoints: GraphQLEndpoint[]): string => {
+    return endpoints
+        .map((endpoint) => {
+            return `    ${endpoint.name}(input: ${endpoint.inputType}): ${endpoint.outputType}`;
+        })
+        .join('\n');
+};
+
+const queriesToSchemaString = (queries: GraphQLEndpoint[]): string => {
+    if (queries.length === 0) {
+        return '';
+    }
+    return `type Query {\n${endpointsToSchemaString(queries)}\n}`;
+};
+
+const mutationsToSchemaString = (mutations: GraphQLEndpoint[]): string => {
+    if (mutations.length === 0) {
+        return '';
+    }
+    return `type Mutation {\n${endpointsToSchemaString(mutations)}\n}`;
+};
+
 const asSchema = (proto: FileDescriptorProto): GraphQLSchema => {
     const types = proto.getMessageTypeList().map(asType);
     const enums = proto.getEnumTypeList().map(asEnum);
-    return { types, enums };
+    const queries: GraphQLEndpoint[] = [];
+    const mutations: GraphQLEndpoint[] = [];
+    return { types, enums, queries, mutations };
 };
 
 const toSchemaString = (schema: GraphQLSchema): string => {
     const withTypes = typesToSchemaString(schema.types);
     const withEnums = enumsToSchemaString(schema.enums);
-    return `${withTypes}\n\n${withEnums}`;
+    const withQueries = queriesToSchemaString(schema.queries);
+    const withMutations = mutationsToSchemaString(schema.mutations);
+    return `${withTypes}\n\n${withEnums}\n\n${withQueries}\n\n${withMutations}`;
 };
 
 export { asSchema, toSchemaString };
